8cfd53b34ed326e6da54cb76e10c7f9b6c55c53f
PFS <peintnerfamilysites@gmail.com>
2026-06-03 11:58:16 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+/* global process */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/PeintnerFamilySites/",
   plugins: [react(), tailwindcss()],
   build: {
-    base: process.env.VITE_BASE_PATH || "/",
     cssCodeSplit: true,
     chunkSizeWarningLimit: 1200,
     rollupOptions: {
